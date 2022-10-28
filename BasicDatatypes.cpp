5656b2b4cb8269a56c48cce642a40aab5f102b33
Shashwat Dixit <75736156+shashwatdixit10@users.noreply.github.com>
2022-10-29 00:44:29 +0530

    Create BasicDatatypes.cpp

---
+++ BasicDatatypes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int c;
+    long d ;
+    char a;
+    float f;
+    double e;
+    cin>>c;
+    cin>>d;
+    cin>>a;
+    cin>>f;
+    cin>>e;
+    
+    cout<<""<<c<<endl;
+    cout<<""<<d<<endl;
+    cout<<""<<a<<endl;
+    cout<<setprecision(3)<<fixed<<f<<endl;
+    cout<<setprecision(9)<<fixed<<e;
+}
